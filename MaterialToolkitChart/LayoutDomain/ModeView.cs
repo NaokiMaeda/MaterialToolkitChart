5f89b0b77c8f965b53298e07b99cff0ebdb6ab4e
Naoki Maeda <maeda.naoki.md9@is.naist.jp>
2016-01-07 19:22:23 +0900

    Separate view xaml

---
+++ MaterialToolkitChart/LayoutDomain/ModeView.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaterialToolkitChart.LayoutDomain {
+	class ModeView {
+		public	String	Name	{set;	get;}
+		public	Object	Content {set;	get;}
+	}
+}
--- MaterialToolkitChart/MainWindow.xaml.cs
+++ MaterialToolkitChart/MainWindow.xaml.cs
@@ -1,5 +1,8 @@
 ﻿using System.Windows;
 using System.Collections.Generic;
+using System;
+using System.Windows.Input;
+
 namespace MaterialToolkitChart {
     /// <summary>
     /// MainWindow.xaml の相互作用ロジック
@@ -12,8 +15,11 @@ namespace MaterialToolkitChart {
             Queue<Data> list = new Queue<Data>();
             list.Enqueue(new Data());
             list.Enqueue(new Data(200, 2017));
-            this.DataView.ItemsSource = list;
 
         }
-    }
+
+		private void UIElement_OnPreviewMouseButtonUp(object sender, MouseButtonEventArgs e) {
+			MenuToggleButton.IsChecked = false;
+		}
+	}
 }
